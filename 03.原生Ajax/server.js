00e30e7a4771c271bfefec727484735f7e6ccd50
cl <1803890878@qq.com>
2023-05-03 20:46:00 +0800

    day1学习

--- 03.原生Ajax/server.js
+++ 03.原生Ajax/server.js
@@ -10,8 +10,45 @@ app.get('/server',(request,response)=>{
     //设置响应头
     response.setHeader('Access-Control-Allow-Origin','*')
     // 设置响应体
-    response.send("halo")
+    response.send("===get=")
 });
+
+app.post('/server',(request,response)=>{
+    //设置响应头
+    response.setHeader('Access-Control-Allow-Origin','*')
+    // 设置响应体
+    response.send("<h1>post</h1>")
+});
+
+app.all('/json-server',(request,response)=>{
+    //设置响应头
+    response.setHeader('Access-Control-Allow-Origin','*')
+    // 设置响应体
+    const data = {
+        name:'chenZi'
+    }
+    let str =JSON.stringify(data)
+    response.send(str)
+});
+
+app.get('/ie',(request,response)=>{
+    //设置响应头
+    response.setHeader('Access-Control-Allow-Origin','*')
+    // 设置响应体
+    response.send('ie')
+});
+
+app.get('/delay',(request,response)=>{
+    //设置响应头
+    response.setHeader('Access-Control-Allow-Origin','*')
+    // 设置响应体
+
+
+    setTimeout(()=>{
+        response.send('延时响应')
+    },1000)
+});
+
 
 // 4. 监听端口启动服务
 app.listen(8000,()=>{
